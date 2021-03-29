aac2a7980f3deb185440b0dd1564fc12973daab8
yumamri <irma.houversing@gmail.com>
2021-03-29 10:58:18 +0200

    Added navigation activity

---
+++ app/src/main/java/com/and/travelbuddy/ui/home/HomeFragment.java
@@ -0,0 +1,25 @@
+package com.and.travelbuddy.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.and.travelbuddy.R;
+
+public class HomeFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        final TextView textView = root.findViewById(R.id.text_home);
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+}
