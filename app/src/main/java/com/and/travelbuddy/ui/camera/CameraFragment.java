640dbc752cd4aa831380ab67b8486a69459f41b5
yumamri <irma.houversing@gmail.com>
2021-04-09 17:30:48 +0200

    I can't access files I don't know why

--- app/src/main/java/com/and/travelbuddy/ui/camera/CameraFragment.java
+++ app/src/main/java/com/and/travelbuddy/ui/camera/CameraFragment.java
@@ -1,6 +1,10 @@
 package com.and.travelbuddy.ui.camera;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +16,12 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.and.travelbuddy.R;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class CameraFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -20,6 +30,54 @@ public class CameraFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_camera, container, false);
         final TextView textView = root.findViewById(R.id.text_camera);
         cameraViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        dispatchTakePictureIntent();
         return root;
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == getActivity().RESULT_OK) {
+            //Bundle extras = data.getExtras();
+            //Bitmap imageBitmap = (Bitmap) extras.get("data");
+            //mImageView.setImageBitmap(imageBitmap);
+            galleryAddPic();
+        }
+    }
+
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        File storageDir = Environment.getExternalStorageDirectory();
+        File image = File.createTempFile(
+                DateFormat.getDateTimeInstance().format(new Date()),  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File photoFile = null;
+        try {
+            photoFile = createImageFile();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        if (photoFile != null) {
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        getActivity().sendBroadcast(mediaScanIntent);
     }
 }
--- app/src/main/java/com/and/travelbuddy/ui/camera/CameraViewModel.java
+++ app/src/main/java/com/and/travelbuddy/ui/camera/CameraViewModel.java
@@ -5,9 +5,7 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class CameraViewModel extends ViewModel {
-
     private MutableLiveData<String> mText;
-
     public CameraViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is camera fragment");
@@ -16,4 +14,5 @@ public class CameraViewModel extends ViewModel {
     public LiveData<String> getText() {
         return mText;
     }
+
 }
