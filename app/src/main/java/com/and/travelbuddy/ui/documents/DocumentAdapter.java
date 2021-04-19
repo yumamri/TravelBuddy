fdb1629d3e184065bea009eebdd4eb7ca57166c0
yumamri <irma.houversing@gmail.com>
2021-04-19 19:13:19 +0200

    scroll view for documents

---
+++ app/src/main/java/com/and/travelbuddy/ui/documents/DocumentAdapter.java
@@ -0,0 +1,64 @@
+package com.and.travelbuddy.ui.documents;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.and.travelbuddy.Document;
+import com.and.travelbuddy.R;
+
+import java.util.ArrayList;
+
+public class DocumentAdapter extends RecyclerView.Adapter<DocumentAdapter.ViewHolder> {
+    private final ArrayList<Document> documentArrayList;
+    final private OnListItemClickListener onListItemClickListener;
+
+
+    public DocumentAdapter(ArrayList<Document> documentArrayList, OnListItemClickListener onListItemClickListener) {
+        this.documentArrayList = documentArrayList;
+        this.onListItemClickListener = onListItemClickListener;
+    }
+
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.document_list_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    public void onBindViewHolder(ViewHolder viewHolder, int position) {
+        viewHolder.title.setText(documentArrayList.get(position).getTitle());
+    }
+
+    public int getItemCount() {
+        return documentArrayList.size();
+    }
+
+    public ArrayList<Document> getDocumentArrayList() {
+        return documentArrayList;
+    }
+
+    public interface OnListItemClickListener {
+        void onListItemClick(int agent);
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        TextView title;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+
+            title = itemView.findViewById(R.id.text_document_title);
+            itemView.setOnClickListener(this);
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            onListItemClickListener.onListItemClick(getAdapterPosition());
+        }
+    }
+}
--- app/src/main/java/com/and/travelbuddy/ui/documents/DocumentsFragment.java
+++ app/src/main/java/com/and/travelbuddy/ui/documents/DocumentsFragment.java
@@ -4,22 +4,46 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.and.travelbuddy.Document;
 import com.and.travelbuddy.R;
 
-public class DocumentsFragment extends Fragment {
+import java.util.ArrayList;
 
+public class DocumentsFragment extends Fragment {
+    RecyclerView recyclerViewAgent;
+    DocumentAdapter documentAdapter;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         DocumentsViewModel documentsViewModel = new ViewModelProvider(this).get(DocumentsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_documents, container, false);
-        final TextView textView = root.findViewById(R.id.text_documents);
+        final TextView textView = root.findViewById(R.id.text_document_title);
         documentsViewModel.text().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+
+
+        recyclerViewAgent = root.findViewById(R.id.recycler_view_document);
+        recyclerViewAgent.hasFixedSize();
+        recyclerViewAgent.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        ArrayList<Document> documentArrayList = new ArrayList<>();
+        documentAdapter = new DocumentAdapter(documentArrayList, this);
+        recyclerViewAgent.setAdapter(documentAdapter);
+    }
+
+    @Override
+    public void onListItemClick(int index) {
+        int documentIndex = index;
+        Document document = documentAdapter.getDocumentArrayList().get(documentIndex);
+        Toast.makeText(getContext(), document.getTitle(), Toast.LENGTH_SHORT).show();
     }
 }
