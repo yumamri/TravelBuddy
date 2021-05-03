3d178dbe49a4e4b9665781a58dbe4b14ebb52e6c
yumamri <irma.houversing@gmail.com>
2021-05-03 19:02:46 +0200

    sign out

--- app/src/main/java/com/and/travelbuddy/ui/main/MainActivity.java
+++ app/src/main/java/com/and/travelbuddy/ui/main/MainActivity.java
@@ -2,6 +2,7 @@ package com.and.travelbuddy.ui.main;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -51,5 +52,9 @@ public class MainActivity extends AppCompatActivity {
     private void startLoginActivity() {
         startActivity(new Intent(this, LoginActivity.class));
         finish();
+    }
+
+    public void signOut() {
+        viewModel.signOut();
     }
 }
