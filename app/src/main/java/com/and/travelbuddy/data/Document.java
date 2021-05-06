ce24ad947155f0a5c56ad0c0504db96d084853f7
yumamri <irma.houversing@gmail.com>
2021-05-06 12:16:14 +0200

    custom dialog for document add

--- app/src/main/java/com/and/travelbuddy/data/Document.java
+++ app/src/main/java/com/and/travelbuddy/data/Document.java
@@ -5,16 +5,14 @@ import com.and.travelbuddy.data.Tag;
 public class Document {
     private String title;
     private Tag category;
-    private String uri;
 
     public Document(String title) {
         this.title = title;
     }
 
-    public Document(String title, Tag category, String uri) {
+    public Document(String title, Tag category) {
         this.title = title;
         this.category = category;
-        this.uri = uri;
     }
 
     public String getTitle() {
@@ -31,13 +29,5 @@ public class Document {
 
     public void setCategory(Tag category) {
         this.category = category;
-    }
-
-    public String getUri() {
-        return uri;
-    }
-
-    public void setUri(String uri) {
-        this.uri = uri;
     }
 }
